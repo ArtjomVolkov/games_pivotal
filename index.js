e88112a254fe525f6783d20a16ee0b225c27beef
Opilane TTHK <opilane@tthk.ee>
2023-10-13 12:19:15 +0300

    Ready

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const port = 8080
 const swaggerUi = require('swagger-ui-express')
-const swaggerDoucemnt = require('./docs/swagger.json');
+const yamljs = require('yamljs')
+const swaggerDoucemnt = yamljs.load('./docs/swagger.yaml');
+
+app.use(express.json())
+
+const games = [
+    {id: 1,name:"CS2",price: 10.99},
+    {id: 2,name:"GTA VI",price: 20.99},
+    {id: 3,name:"CS GO",price: 30.99}
+]
 
 app.get('/games',(req,res)=>{
-    res.send(["CS2","GTA VI"])
+    res.send(games)
+})
+
+app.get('/games/:id',(req,res)=>{
+    if(typeof games[req.params.id - 1]==='undefined'){
+        return res.status(404).send({error: "Game not found"})
+    }
+
+    res.send(games[req.params.id -1])
+})
+
+
+app.post('/games',(req,res)=>{
+    if(!req.body.name || !req.body.price){
+        return res.status(400).send({error:'One or all pararms'})
+    }
+    let game ={
+        id: games.length + 1,
+        price: req.body.price,
+        name: req.body.name
+    }
+
+    games.push(game)
+
+    res.status(201)
+        .location(`${getBaseUrl(req)}/games/${games.length}}`)
+        .send(game)
+})
+
+app.delete('/games/:id',(req,res)=>{
+    if (typeof games[req.params.id - 1]==='undefined'){
+        return res.status(404).send({error:'Game not found'})
+    }
+
+    games.splice(req.params.id - 1,1)
+
+    res.status(204).send({error:"No content"})
 })
 
 app.use('/docs',swaggerUi.serve,swaggerUi.setup(swaggerDoucemnt))
@@ -12,3 +58,8 @@ app.use('/docs',swaggerUi.serve,swaggerUi.setup(swaggerDoucemnt))
 app.listen(port,()=>{
     console.log(`Api up at: http://localhost:${port}`)
 })
+
+function getBaseUrl(req){
+    return req.connection && req.connection.encrypted
+    ? 'https' : 'http' + `://${req.headers.host}`
+}
